10911a810c3209bd6a435d932fb8e59e686d5879
MohdTauseefKhan <mohdtousifkhan78@gmail.com>
2026-05-09 12:27:51 +0530

    Started with Backend

--- Cartify-Frontend/src/Components/Product.jsx
+++ Cartify-Frontend/src/Components/Product.jsx
@@ -1,105 +1,38 @@
+import axios from "axios";
 import ProductCard from "./ProductCard";
+import { useState, useEffect } from "react";
 
 const Product = () => {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    const products = async () => {
+      const response = await axios.get("http://localhost:8080/products");
+
+      setProduct(response.data);
+    };
+
+    products();
+  }, []);
+
   return (
     <div className="min-h-screen bg-zinc-100 p-4 sm:p-6 lg:p-10">
       <div className="mx-auto grid max-w-7xl place-items-center grid-cols-2 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <ProductCard
-          name="Minimal Leather Sneakers"
-          category="Footwear"
-          price={3499}
-          oldPrice={4999}
-          rating={4.8}
-          reviews={126}
-          image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=800&q=80"
-          badge="Best Seller"
-          onAddToCart={() => console.log("Added to cart")}
-          onWishlist={() => console.log("Wishlist")}
-        />
-        <ProductCard
-          name="Minimal Leather Sneakers"
-          category="Footwear"
-          price={3499}
-          oldPrice={4999}
-          rating={4.8}
-          reviews={126}
-          image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=800&q=80"
-          badge="Best Seller"
-          onAddToCart={() => console.log("Added to cart")}
-          onWishlist={() => console.log("Wishlist")}
-        />
-        <ProductCard
-          name="Minimal Leather Sneakers"
-          category="Footwear"
-          price={3499}
-          oldPrice={4999}
-          rating={4.8}
-          reviews={126}
-          image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=800&q=80"
-          badge="Best Seller"
-          onAddToCart={() => console.log("Added to cart")}
-          onWishlist={() => console.log("Wishlist")}
-        />
-        <ProductCard
-          name="Minimal Leather Sneakers"
-          category="Footwear"
-          price={3499}
-          oldPrice={4999}
-          rating={4.8}
-          reviews={126}
-          image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=800&q=80"
-          badge="Best Seller"
-          onAddToCart={() => console.log("Added to cart")}
-          onWishlist={() => console.log("Wishlist")}
-        />
-        <ProductCard
-          name="Minimal Leather Sneakers"
-          category="Footwear"
-          price={3499}
-          oldPrice={4999}
-          rating={4.8}
-          reviews={126}
-          image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=800&q=80"
-          badge="Best Seller"
-          onAddToCart={() => console.log("Added to cart")}
-          onWishlist={() => console.log("Wishlist")}
-        />
-        <ProductCard
-          name="Minimal Leather Sneakers"
-          category="Footwear"
-          price={3499}
-          oldPrice={4999}
-          rating={4.8}
-          reviews={126}
-          image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=800&q=80"
-          badge="Best Seller"
-          onAddToCart={() => console.log("Added to cart")}
-          onWishlist={() => console.log("Wishlist")}
-        />
-        <ProductCard
-          name="Minimal Leather Sneakers"
-          category="Footwear"
-          price={3499}
-          oldPrice={4999}
-          rating={4.8}
-          reviews={126}
-          image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=800&q=80"
-          badge="Best Seller"
-          onAddToCart={() => console.log("Added to cart")}
-          onWishlist={() => console.log("Wishlist")}
-        />
-        <ProductCard
-          name="Minimal Leather Sneakers"
-          category="Footwear"
-          price={3499}
-          oldPrice={4999}
-          rating={4.8}
-          reviews={126}
-          image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=800&q=80"
-          badge="Best Seller"
-          onAddToCart={() => console.log("Added to cart")}
-          onWishlist={() => console.log("Wishlist")}
-        />
+        {product.map((p, key) => (
+          <ProductCard
+            key={key}
+            name={p.name}
+            category={p.category}
+            price={p.price}
+            oldPrice={p.oldPrice}
+            rating={p.rating}
+            reviews={p.review}
+            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=800&q=80"
+            badge={p.badge}
+            onAddToCart={() => console.log("Added to cart")}
+            onWishlist={() => console.log("Wishlist")}
+          />
+        ))}
       </div>
     </div>
   );
